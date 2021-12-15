7c4a0ac8ff485feed35ce93b018d7a0b06c556c0
sruthy-ps <94426718+sruthy-ps@users.noreply.github.com>
2021-12-15 14:31:35 +0530

    Add files via upload

---
+++ iambdaa.py
@@ -0,0 +1,7 @@
+import math
+s_area=lambda a:a**2
+r_area=lambda len,ht:len*ht
+t_area=lambda b,h:(1/2)*b*h
+print("area of square(10)is:",s_area(10))
+print("areaof rectangle(30,20)is:",r_area(30,20))
+print("area of triangle(10,20)is:",t_area(10,20))
